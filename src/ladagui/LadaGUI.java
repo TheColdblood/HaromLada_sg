6f97c3e4614b9405e052143f79c6f25cb2378911
logan114 <kepirobalazs204@gmail.com>
2024-09-11 14:45:45 +0200

    GUI kep

--- src/ladagui/LadaGUI.java
+++ src/ladagui/LadaGUI.java
@@ -74,6 +74,7 @@ public class LadaGUI extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
